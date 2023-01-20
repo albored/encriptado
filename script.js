3a883eefb486ee5518c830cca47c267329dd05bb
Alvaro Cayo <96433864+albored@users.noreply.github.com>
2023-01-20 16:47:52 -0400

    fix

--- script.js
+++ script.js
@@ -1,60 +1,70 @@
-const textInput = document.querySelector(".textarea-input");
-const textOut = document.querySelector(".result-out");
+const textInput = document.getElementById("textarea");
 const btnEncriptar = document.querySelector(".btn-encriptar");
-const btnDesencriptar = document.querySelector(".btn-desencriptar");
-const imgEl = document.querySelector(".img");
-const btnCopy = document.querySelector(".btn-copy");
-const resultBox = document.querySelector(".result-div");
-const errorMsg = document.querySelector(".error");
-const about = document.querySelector(".btn-info");
-const container = document.querySelector(".container");
-const containerBox = document.querySelector(".container-box");
+const displayImg = document.querySelector(".display-out-img");
+const displayResult = document.querySelector(".display-out-result");
+const textResult = document.querySelector(".display-result");
+const btnCopyClear = document.querySelector(".btn-copy");
+const btnDecript = document.querySelector(".btn-decript");
+const textError = document.querySelector(".text-warning");
+const btnHome = document.querySelector(".btn-home");
+const about = document.querySelector(".about");
+const aboutContainer = document.querySelector(".about-container");
+const mainDiv = document.querySelector(".main");
 
 btnEncriptar.addEventListener("click", function () {
   if (textInput.value === "") return;
-  if (textInput.value.split("").some((l) => l === +l)) {
-    textInput.value = "";
-    return textError();
-  }
 
-  const data = textInput.value.split("");
+  const data = textInput.value;
   textInput.value = "";
 
-  const dictionary = "abcdefghijklmnñopqrstuvwxyz ".split("");
-  const dataUpperCase = data.every((el) => el === el.toUpperCase());
+  if (checkData(data.split(""))) {
+    const result = encriptarText(data.split(""));
+    displayResultBox();
+    textResult.textContent = result;
 
-  const dataSymbol = data.every((el) =>
-    dictionary.some((letra) => letra === el)
-  );
-  console.log(dataUpperCase);
-
-  if (!dataUpperCase && dataSymbol) handleEncriptar(data);
-  else textError();
+    btnCopyClear.addEventListener("click", function () {
+      copyclear();
+      copyData(result);
+      textResult.textContent = " ";
+    });
+  } else inputError();
 });
 
-btnDesencriptar.addEventListener("click", function () {
+btnDecript.addEventListener("click", function () {
   if (textInput.value === "") return;
-  let data = textInput.value.replaceAll("enter", "e");
-  data = data.replaceAll("imes", "i");
-  data = data.replaceAll("ai", "a");
-  data = data.replaceAll("ober", "o");
-  data = data.replaceAll("ufat", "u");
+  const data = textInput.value;
+  textInput.value = "";
 
-  textOut.textContent = data;
-  renderResult();
-
-  btnCopy.addEventListener("click", function () {
-    copyText(data);
-    textOut.textContent = "";
-    renderImg();
-  });
-
-  renderResult();
+  if (checkData(data.split(""))) {
+    const result = decriptText(data);
+    displayResultBox();
+    textResult.textContent = result;
+  } else inputError();
 });
 
-function handleEncriptar(data) {
-  const msgEncriptado = data.map((el) => {
-    switch (el) {
+about.addEventListener("click", function (e) {
+  e.preventDefault();
+  mainDiv.classList.add("hidden");
+  aboutContainer.classList.remove("hidden");
+});
+
+btnHome.addEventListener("click", function () {
+  aboutContainer.classList.add("hidden");
+  mainDiv.classList.remove("hidden");
+});
+
+function checkData(data) {
+  const dictionary = "abcefghijklmnñopqrstuvwxyz ".split("");
+
+  const checkUpper = data.some((word) => word.includes(word.toUpperCase()));
+  const checkSymbol = data.every((word) => dictionary.includes(word));
+
+  return checkSymbol;
+}
+
+function encriptarText(data) {
+  const text = data.map((word) => {
+    switch (word) {
       case "e":
         return "enter";
       case "i":
@@ -66,69 +76,41 @@ function handleEncriptar(data) {
       case "u":
         return "ufat";
       default:
-        return el;
+        return word;
     }
   });
-  const copyData = (textOut.textContent = msgEncriptado.join(""));
 
-  btnCopy.addEventListener("click", function () {
-    copyText(copyData);
-    textOut.textContent = "";
-    renderImg();
-  });
-
-  renderResult();
+  return text.join("");
 }
 
-function renderResult() {
-  imgEl.classList.add("hidden");
-  textOut.classList.remove("hidden");
-  btnCopy.classList.remove("hidden");
-  resultBox.classList.remove("hidden");
+function decriptText(data) {
+  let text = data.replaceAll("enter", "e");
+  text = text.replaceAll("imes", "i");
+  text = text.replaceAll("ai", "a");
+  text = text.replaceAll("ober", "o");
+  text = text.replaceAll("ufat", "u");
+
+  return text;
 }
 
-function renderImg() {
-  imgEl.classList.remove("hidden");
-  textOut.classList.add("hidden");
-  btnCopy.classList.add("hidden");
-  resultBox.classList.add("hidden");
+function displayResultBox() {
+  displayImg.classList.add("hidden");
+  displayResult.classList.remove("hidden");
 }
 
-function copyText(text) {
+function copyclear() {
+  displayImg.classList.remove("hidden");
+  displayResult.classList.add("hidden");
+}
+
+function copyData(text) {
   navigator.clipboard.writeText(text);
 }
 
-function textError() {
-  errorMsg.style.opacity = "1";
+function inputError() {
+  textError.classList.remove("hidden");
+
   setTimeout(() => {
-    errorMsg.style.opacity = "0";
+    textError.classList.add("hidden");
   }, 2000);
 }
-
-about.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  containerBox.classList.add("hidden");
-
-  const html = `
-      <div class="info">
-        <p><span>Version: </span> 1.0.0</p>
-        <p>
-          <span>Descripcion:</span> Challenge de
-          <a class="btn-oracle" href="https://www.aluracursos.com/" target="_blank"
-            >Oracle - Alura.</a>
-          Encriptador y desencriptador de Palabras.
-        </p>
-        <p><span>Autor:</span> Alvaro Cayo  <a class="btn-oracle" href="https://github.com/albored" target="_blank"><ion-icon name="logo-github"></ion-icon></a></p>
-        <button class="btn-about btn"> <ion-icon name="home-outline"></ion-icon> Back to Home</button>
-      </div>`;
-
-  container.insertAdjacentHTML("afterbegin", html);
-
-  const info = document.querySelector(".info");
-  const btnAbout = document.querySelector(".btn-about");
-  btnAbout.addEventListener("click", function () {
-    info.classList.add("hidden");
-    containerBox.classList.remove("hidden");
-  });
-});
